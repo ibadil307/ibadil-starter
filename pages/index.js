c975cb7439801a2cd96b61c2fe6d303daea1f01b
ibadil307 <43997372+ibadil307@users.noreply.github.com>
2022-09-07 22:56:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,23 @@
-<html>
-<head>
-<title>Contoh Biodata HTML</title>
-</head>
-<body>
-<fieldset class="v" />
-<h2 align="center">TENTANG ANAKTEKNO.COM</h2>
-<center><img src="images.jpg" width="300px" height="200px" border="5"/></center>
-<PRE>
-ID : 31072022<BR>
-NAMA : Anak Tekno <br>
-ANGKATAN : Tahun 99<sup>2017</sup>
-</PRE>
-<hr width="100" align="right">
-<PRE>
-NO. HP : 085396717324<BR>
-Blog : <a href="https://www.anaktekno.com" target="_blank">Anaktekno.com</a>
-</PRE>
-<hr SIZE ="12PX" COLOR= "yellow" = "center"><PRE>
-TEMPAT :<FONT COLOR ="red"> Palopo, <sub>tgl</sub>Berdiri : 31 Juli 2021</FONT><br>
-NICHE :<FONT COLOR ="blue"> Teknologi</FONT> <BR>
-SUB NICHE :<FONT COLOR ="blue"> Fintech, etc.</FONT> </PRE>
-<p>Dengan ini menyatakan Anaktekno.com sebagai blog pribadi yang membahas tentang tutorial terbaru tentang
-<b>teknologi</b>.<br></p>
-<p align ="right">Palopo, 31 Juli 2021 <br>
-Tertanda, </p> <br>
-<p align ="right"><u>Anak Tekno</U><br>
-<SUB>31072021</SUB></p>
-<footer>
-<marquee><font color="green" font-size="small">Copyright&copy; 2021
-ANAKTEKNO.COM</font></marquee>
-</footer>
-</body>
-</html> 
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Test</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Muhammad ibadil mukhlasin 
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
